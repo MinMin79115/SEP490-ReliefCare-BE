1ece6cefbeb322e62d2ffef1a7c001b294d2987d
MinMint <minmintworkspace@gmail.com>
2026-01-08 22:36:44 +0700

    Update GenericRepository to use ApplicationDbContext
    Changed the GenericRepository to depend on ApplicationDbContext instead of the generic DbContext. This ensures repository operations are tightly coupled with the application's specific database context.

--- ReliefManagementSystem.Infrastructure/Repositories/GenericRepository.cs
+++ ReliefManagementSystem.Infrastructure/Repositories/GenericRepository.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ReliefManagementSystem.Application.Common.Interface;
+using ReliefManagementSystem.Infrastructure.Context;
 
 namespace ReliefManagementSystem.Infrastructure.Repositories
 {
     public class GenericRepository<T> : IGenericRepository<T>
         where T : class
     {
-        protected readonly DbContext _context;
+        protected readonly ApplicationDbContext _context;
         protected readonly DbSet<T> _dbSet;
 
-        public GenericRepository(DbContext context)
+        public GenericRepository(ApplicationDbContext context)
         {
             _context = context;
             _dbSet = _context.Set<T>();
