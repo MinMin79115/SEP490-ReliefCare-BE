a5d486fa335d5f70438b90df3ae9dc75eb0768e8
tranduy <tranduy256789@gmail.com>
2026-01-16 17:55:13 +0700

    change ireadonly

--- ReliefManagementSystem.Application/Common/Interface/IGenericRepository.cs
+++ ReliefManagementSystem.Application/Common/Interface/IGenericRepository.cs
@@ -3,7 +3,7 @@
     public interface IGenericRepository<T> where T : class
     {
         Task<T?> GetByIdAsync(Guid id);
-        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<List<T>> GetAllAsync();
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);
