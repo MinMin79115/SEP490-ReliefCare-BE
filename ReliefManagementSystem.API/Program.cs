26ac0643e33951f90561595280830a43f960ad0c
MinMint <minmintworkspace@gmail.com>
2026-02-03 17:36:08 +0700

    Update Program.cs

--- ReliefManagementSystem.API/Program.cs
+++ ReliefManagementSystem.API/Program.cs
@@ -141,7 +141,8 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Enable Swagger for Development and Staging
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
