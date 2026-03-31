591602baf3f93f50aa7634770a754d914fe9e414
LOSER <tranduy256789@gmail.com>
2026-03-31 21:15:37 +0700

    get All Moderator

---
+++ ReliefManagementSystem.Application/Features/User/ModeratorProfileResponse.cs
@@ -0,0 +1,23 @@
+using ReliefManagementSystem.Domain.Enum;
+
+namespace ReliefManagementSystem.Application.Features.User
+{
+    public class ModeratorProfileResponse
+    {
+        public Guid Id { get; set; }
+        public string? DisplayName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? PictureUrl { get; set; }
+
+        public bool IsBanned { get; set; }
+        public DateTimeOffset? LockoutEnd { get; set; }
+        public string? BanReason { get; set; }
+
+        public ModeratorStatus? ModeratorStatus { get; set; }
+        public bool IsStationHead { get; set; }
+        public bool IsManagingStation { get; set; }
+        public Guid? ReliefStationId { get; set; }
+        public string? ReliefStationName { get; set; }
+    }
+}
